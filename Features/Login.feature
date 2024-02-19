e5a2bed1ed48d8009720259389a1d83dff24cfbe
piyushr1987 <piyushr219@gmail.com>
2024-02-19 17:46:59 +0530

    login feature with DDT has been added

--- Features/Login.feature
+++ Features/Login.feature
@@ -7,5 +7,35 @@ Feature: Login
     And click on login
     Then User should navigate to Swag labs home page and URL should be "https://www.saucedemo.com/inventory.html".
     And close the browser
+    
+    Scenario: Successful Login with Valid Credentials DDT
+    Given User Launch Chrome browser 
+    When user open the URL "https://www.saucedemo.com/"
+    And user enter the username as "<username>" and password as "<password>"
+    And click on login
+    Then User should navigate to Swag labs home page and URL should be "https://www.saucedemo.com/inventory.html".
+    And close the browser
+    
+    Examples:
+|username|password|
+|standard_user|secret_sauce|
+|problem_user|secret_sauce|
+|problem_user|admin123|
+
+ Scenario: Display login error messages
+    Given User Launch Chrome browser 
+    When user open the URL "https://www.saucedemo.com/"
+    And user enter the username as "" and password as ""
+    And click on login
+    Then User should see the correct error message as "Epic sadface: Username is required".
+    And close the browser
+    
+    Scenario: Display login error messages
+    Given User Launch Chrome browser 
+    When user open the URL "https://www.saucedemo.com/"
+    And user enter the username as "standard_user" and password as ""
+    And click on login
+    Then User should see the correct error message for password field as "Epic sadface: Password is required"
+    And close the browser
 
  
--- src/test/java/stepDefinition/StepDef.java
+++ src/test/java/stepDefinition/StepDef.java
@@ -1,15 +1,23 @@
 package stepDefinition;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -76,6 +84,34 @@ public class StepDef extends BaseClass {
 		log.info("login button clicked");
 	}
 
+	@Then("User should see the correct error message as {string}.")
+	public void user_should_see_the_correct_error_message_as(String actErrorMessage) {
+
+		WebElement errorMessage = driver.findElement(By.xpath("//div[@class='login-box']/descendant::h3"));
+		String expErrorMessage = errorMessage.getText();
+		if (actErrorMessage.equalsIgnoreCase(expErrorMessage)) {
+			log.warn("Test Passed:login page error message is matched....");
+			Assert.assertTrue(true);
+		} else {
+			log.warn("Test Failed:login page error message is not matched....");
+			Assert.assertTrue(false);
+		}
+	}
+	@Then("User should see the correct error message for password field as {string}")
+	public void user_should_see_the_correct_error_message_for_password_field_as(String actErrorMessage) {
+		
+		WebElement errorMessage = driver.findElement(By.xpath("//div[@class='login-box']/descendant::h3"));
+		String expErrorMessage = errorMessage.getText();
+		if (actErrorMessage.equalsIgnoreCase(expErrorMessage)) {
+			log.warn("Test Passed:login page error message is matched....");
+			Assert.assertTrue(true);
+		} else {
+			log.warn("Test Failed:login page error message is not matched....");
+			Assert.assertTrue(false);
+		}
+	    
+	}
+
 	@Then("User should navigate to Swag labs home page and URL should be {string}.")
 	public void user_should_navigate_to_swag_labs_home_page_and_url_should_be(String actURL) {
 
@@ -89,12 +125,35 @@ public class StepDef extends BaseClass {
 		}
 	}
 
+	@After()
+	public void tearDown(Scenario sc) {
+		System.out.println("Tear down method is executed");
+		if (sc.isFailed() == true) {
+			String fileWithPath = "C:\\Users\\piyush ramteke\\Piyush-Workspace\\SwagLabs_Cucumber\\Screenshots\\failedScreenshot.png";
+			TakesScreenshot scrShot = ((TakesScreenshot) driver);
+
+			// Call getScreenshotAs method to create image file
+			File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+
+			// Move image file to new destination
+			File DestFile = new File(fileWithPath);
+
+			// Copy file at destination
+
+			try {
+				FileUtils.copyFile(SrcFile, DestFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+		driver.quit();
+	}
+
 	@Then("close the browser")
 	public void close_the_browser() {
 
-		driver.close();
-		// driver.quit();
-
+		driver.quit();
 		log.info("browser is closed");
 
 	}
